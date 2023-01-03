67224389fc2bdc1ef1d8a6580719af9b7efa08e0
瑜 <58211460+QQ1010@users.noreply.github.com>
2023-01-03 08:36:06 +0000

    Add：weather

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from __future__ import unicode_literals
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, MessageTemplateAction, PostbackEvent, PostbackTemplateAction, PostbackAction
 import configparser
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -37,11 +39,62 @@ def callback():
     return 'OK'
 
 
-# echo 回話
+# 設定回復
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def handle_weahter_message(event):
+    if isinstance(event, MessageEvent):
+        print("message event")
+        if event.message.text == "天氣":
+            line_bot_api.reply_message(  # 回復傳入的訊息文字
+                event.reply_token,
+                TemplateSendMessage(alt_text='Buttons template',
+                                    template=ButtonsTemplate(
+                                        title='查詢天氣地點',
+                                        text='請選擇地區',
+                                        actions=[
+                                            PostbackAction(label='臺北市',
+                                                           data='臺北市'),
+                                            PostbackAction(label='彰化縣',
+                                                           data='彰化縣'),
+                                        ])))
+        else:
+            line_bot_api.reply_message(event.reply_token,
+                                   TextSendMessage(text=event.message.text))
+
+
+@handler.add(PostbackEvent)
+def handle_weather_postback(event):
+    weather = reply_weather(event.postback.data)
     line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text=event.message.text))
+                               TextSendMessage(text=weather))
+
+
+def reply_weather(location_name):
+    request_URL = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001"
+    params = {
+        "Authorization": config.get('weather', 'access_token'),
+        "locationName": location_name,
+    }
+    response = requests.get(request_URL, params=params)
+    if (response.status_code == 200):
+        data = json.loads(response.text)
+        # print(data["records"])
+        location = data["records"]["location"][0]["locationName"]
+        weather_elements = data["records"]["location"][0]["weatherElement"]
+        # start_time = weather_elements[0]["time"][0]["startTime"]
+        # end_time = weather_elements[0]["time"][0]["endTime"]
+        weather_state = weather_elements[0]["time"][0]["parameter"][
+            "parameterName"]
+        rain_prob = weather_elements[1]["time"][0]["parameter"][
+            "parameterName"]
+        min_tem = weather_elements[2]["time"][0]["parameter"]["parameterName"]
+        comfort = weather_elements[3]["time"][0]["parameter"]["parameterName"]
+        max_tem = weather_elements[4]["time"][0]["parameter"]["parameterName"]
+        weather_result = "地點🌏： " + location + "\n今日天氣狀況⛅： " + weather_state + "\n今日降雨機率☔： " + rain_prob + "\n最高溫度： " + max_tem + "\n最低溫度： " + min_tem + "\n體感溫度狀況： " + comfort
+        return weather_result
+    else:
+        print("Can't get data.")
+        return "Can't get weather data"
 
 
 if __name__ == "__main__":
